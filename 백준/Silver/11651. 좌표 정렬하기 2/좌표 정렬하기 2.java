73eb093e078e26c30e1d667198465b51a33a4ff0
이소영 | Lee Soyoung <72489644+gitSoyoungLee@users.noreply.github.com>
2025-11-26 21:52:00 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 720 ms, Memory: 58360 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int total = Integer.parseInt(br.readLine());
+
+    int[][] arr = new int[total][2];
+    for(int i=0; i<total; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      arr[i][0] = Integer.parseInt(st.nextToken());
+      arr[i][1] = Integer.parseInt(st.nextToken());
+    }
+    
+    Arrays.sort(arr, (e1, e2) -> {
+      if(e1[1] == e2[1]) {
+        return e1[0] - e2[0]; 
+      } else return e1[1] - e2[1];
+    });
+
+    StringBuilder sb = new StringBuilder();
+    for(int i=0; i<total; i++) {
+      sb.append(arr[i][0] + " " + arr[i][1] + "\n");
+    }
+
+    System.out.println(sb.toString());
+  }
+}
